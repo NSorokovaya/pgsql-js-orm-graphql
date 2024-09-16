ba2b7fb34b643c55bd5effc8d14b99ece82beaa4
NSorokovaya <nadiaasorokovaya@gmail.com>
2024-09-16 11:54:00 +0200

    added authorization

--- express-prisma-graphql/src/index.ts
+++ express-prisma-graphql/src/index.ts
@@ -1,9 +1,12 @@
 import { readFileSync } from "node:fs";
 import path from "node:path";
+import jwt from "jsonwebtoken";
 
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { PrismaClient } from "@prisma/client";
+import { authMiddleware } from "./middleware";
+import { Context } from "node:vm";
 
 const prisma = new PrismaClient();
 
@@ -11,24 +14,72 @@ const typeDefs = readFileSync(path.join(__dirname, "schema.graphql"), "utf8");
 
 const resolvers = {
   Query: {
-    users: () =>
+    users: (context: Context) => {
+      if (!context.user) throw new Error("Not authorized");
       prisma.users.findMany({
         include: { posts: true },
-      }),
-    user: (_parent: any, args: { id: string }) =>
-      prisma.users.findUnique({ where: { id: args.id } }),
-    chats: (_parent: any, args: { id: string }) => prisma.chats.findMany(),
-    posts: () => prisma.posts.findMany(),
-    post: (_parent: any, args: { id: string }) =>
-      prisma.posts.findUnique({ where: { id: args.id } }),
+      });
+    },
+    user: (_parent: any, args: { id: string }, context: Context) => {
+      if (!context.user) throw new Error("Not authorized");
+      prisma.users.findUnique({ where: { id: args.id } });
+    },
+
+    chats: (_parent: any, args: { id: string }, context: Context) => {
+      if (!context.user) throw new Error("Not authorized");
+      prisma.chats.findMany();
+    },
+    posts: (context: Context) => {
+      if (!context.user) throw new Error("Not authorized");
+      prisma.posts.findMany();
+    },
+    post: (_parent: any, args: { id: string }, context: Context) => {
+      if (!context.user) throw new Error("Not authorized");
+      prisma.posts.findUnique({
+        where: { id: args.id },
+        include: { users: true },
+      });
+    },
+  },
+  Post: {
+    user: async (post: any, _: any, context: Context) => {
+      if (!context.user) throw new Error("Not authorized");
+
+      return await prisma.users.findUnique({
+        where: { id: post.user_id },
+        include: { posts: true },
+      });
+    },
+  },
+  User: {
+    posts: async (
+      user: any,
+      _: any,
+      { parent }: { parent: any },
+      context: Context
+    ) => {
+      if (!context.user) throw new Error("Not authorized");
+
+      return await prisma.posts.findMany({
+        where: {
+          user_id: user.id,
+        },
+      });
+    },
   },
   Mutation: {
-    createUser: (_parent: any, args: { username: string; password: string }) =>
-      prisma.users.create({ data: args }),
+    createUser: (
+      _parent: any,
+      args: { username: string; password: string }
+    ) => {
+      return prisma.users.create({ data: args });
+    },
     createPost: async (
       _parent: any,
-      args: { userId: string; title: string; content: string }
+      args: { userId: string; title: string; content: string },
+      context: Context
     ) => {
+      if (!context.user) throw new Error("Not authorized");
       const post = await prisma.posts.create({
         data: {
           title: args.title,
@@ -43,13 +94,36 @@ const resolvers = {
         content: post.content,
       };
     },
+    login: async (
+      _parent: any,
+      args: { username: string; password: string }
+    ) => {
+      const user = await prisma.users.findFirst({
+        where: { username: args.username },
+      });
+      if (!user || user.password !== args.password) {
+        throw new Error("Password is wrong");
+      }
+      const token = jwt.sign({ userId: user.id }, "secret-key", {
+        expiresIn: "12h",
+      });
+      return { token, user };
+    },
   },
 };
 
 async function startApolloServer() {
   const app = express();
+  app.use(authMiddleware);
 
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      // @ts-ignore
+      return { user: req.user };
+    },
+  });
 
   await server.start();
 
